a0d250326532bf21448bb248c44f968540da0ec4
pumelotea <535553297@qq.com>
2024-05-07 21:43:50 +0800

    feat: 初步实现

--- packages/lib/src/editor/index.js
+++ packages/lib/src/editor/index.js
@@ -1,6 +1,7 @@
 import Editor from './Editor.vue'
 function install(app) {
   app.component('VividEditor', Editor)
+  app.component('viv-editor', Editor)
 }
 Editor.install = install
 export default Editor
