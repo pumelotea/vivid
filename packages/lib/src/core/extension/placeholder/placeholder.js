5a625d64805675903d91b8cd5e8f9a714748f739
pumelotea <535553297@qq.com>
2024-05-24 15:33:52 +0800

    feat: 新增trailing-node

--- packages/lib/src/core/extension/placeholder/placeholder.js
+++ packages/lib/src/core/extension/placeholder/placeholder.js
@@ -3,16 +3,19 @@ import Placeholder from '@tiptap/extension-placeholder'
 export function usePlaceholder(options) {
 	if (!options) {
 		options = {
-			// Use a placeholder:
-			placeholder: '写点有意思的内容...',
-			// Use different placeholders depending on the node type:
-			// placeholder: ({ node }) => {
-			//   if (node.type.name === 'heading') {
-			//     return 'What’s the title?'
-			//   }
-
-			//   return 'Can you add some further context?'
-			// },
+			placeholder: ({ node, pos }) => {
+				console.log(node.type?.name === 'heading')
+				if (node?.type?.name === 'heading') {
+					return `标题${node.attrs.level}`
+				}
+				if (node?.type?.name === 'codeBlock') {
+					return ''
+				}
+				if (pos === 0) {
+					return '请输入正文'
+				}
+				return `输入 '/' 唤起更多`
+			},
 		}
 	}
 	return Placeholder.configure(options)
