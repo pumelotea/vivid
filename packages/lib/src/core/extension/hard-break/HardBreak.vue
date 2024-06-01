fc0fc5c2460397e30ffc3d7757f1d38198775bbd
pumelotea <535553297@qq.com>
2024-06-01 15:20:05 +0800

    feat(hard-break): rewrite to ts

--- packages/lib/src/core/extension/hard-break/HardBreak.vue
+++ packages/lib/src/core/extension/hard-break/HardBreak.vue
@@ -1,23 +1,18 @@
-<script setup>
-import HardBreak from '@tiptap/extension-hard-break'
-import { inject } from 'vue'
+<script setup lang="ts">
+  import HardBreak, { HardBreakOptions } from "@tiptap/extension-hard-break";
+  import { PropType } from "vue";
+  import { injectExtension } from "../utils/common";
 
-const props = defineProps({
-	options: {
-		type: Object,
-		required: false,
-	},
-})
+  const props = defineProps({
+    options: {
+      type: Object as PropType<Partial<HardBreakOptions>>,
+      required: false,
+    },
+  });
 
-const editorInstance = inject('editorInstance')
-const useExtension = inject('useExtension')
-if (!useExtension) {
-	throw new Error('HardBreak component must under VividEditor menu slot')
-}
-useExtension(HardBreak.configure(props.options))
+  injectExtension(HardBreak.configure(props.options));
 </script>
 
 <template>
-	<div style="display: none"></div>
 </template>
 <style scoped></style>
--- packages/lib/src/core/extension/hard-break/index.js
+++
@@ -1,8 +0,0 @@
-import HardBreak from '@tiptap/extension-hard-break'
-import HardBreakExt from './HardBreak.vue'
-
-export function useHardBreak(options) {
-	return HardBreak.configure(options)
-}
-
-export { HardBreakExt }
