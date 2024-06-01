487c5795c8b45d2d3b1db69ce7f49c9e594433c5
pumelotea <535553297@qq.com>
2024-06-01 15:18:19 +0800

    feat(fullscreen): rewrite to ts

--- packages/lib/src/core/extension/fullscreen/Fullscreen.vue
+++ packages/lib/src/core/extension/fullscreen/Fullscreen.vue
@@ -1,28 +1,29 @@
-<script setup>
-import VividMenuItem from '../../components/VividMenuItem.vue'
-import { inject } from 'vue'
+<script setup lang="ts">
+  import VividMenuItem from "../../components/VividMenuItem.vue";
+  import { useEditorInstance } from "../utils/common";
 
-const editorInstance = inject('editorInstance')
-function toggleFullscreen() {
-	editorInstance.value.storage.fullscreen.value =
-		!editorInstance.value.storage.fullscreen.value
-}
+  const editorInstance = useEditorInstance();
+
+  function toggleFullscreen() {
+    editorInstance.value.storage.fullscreen.value =
+      !editorInstance.value.storage.fullscreen.value;
+  }
 </script>
 
 <template>
-	<div v-if="editorInstance">
-		<slot>
-			<vivid-menu-item
-				:icon="
+  <div v-if="editorInstance">
+    <slot>
+      <vivid-menu-item
+        :icon="
 					editorInstance.storage.fullscreen.value
 						? 'fullscreen-exit-line'
 						: 'fullscreen-line'
 				"
-				title="全屏"
-				:action="toggleFullscreen"
-			/>
-		</slot>
-	</div>
+        title="全屏"
+        :action="toggleFullscreen"
+      />
+    </slot>
+  </div>
 </template>
 
 <style scoped></style>
