c8794e5505781884896a3d988b7c4ccc8addc84f
pumelotea <535553297@qq.com>
2024-06-03 14:50:53 +0800

    feat(drag-handle): rewrite to ts

--- packages/lib/src/core/extension/drag-handle/DragHandle.vue
+++ packages/lib/src/core/extension/drag-handle/DragHandle.vue
@@ -1,283 +1,273 @@
 <script setup lang="ts">
-import {NButton, NPopover, NElement, useThemeVars} from 'naive-ui'
-import {useDragHandle} from './drag-handle.js'
-import {inject, onMounted, ref, watch} from "vue"
-import {Editor} from '@tiptap/core'
+	import { NPopover, NElement, useThemeVars } from "naive-ui";
+	import { lockDragHandle, unlockDragHandle, useDragHandle, useDragHandleData } from "./drag-handle";
+	import { onMounted, ref, watch } from "vue";
+	import { Range } from "@tiptap/core";
+	import { injectExtension, useEditorInstance } from "@lib/core/extension/utils/common";
 
-const vars = useThemeVars()
-const root = ref()
-const showSlash = ref(false)
-const showPop = ref(false)
+	const vars = useThemeVars();
+	const root = ref<any>();
+	const showSlash = ref(false);
+	const showPop = ref(false);
 
-const editorInstance: any = inject('editorInstance')!
-const useExtension: any = inject('useExtension')
-if (!useExtension) {
-  throw new Error('DragHandle component must under VividEditor menu slot')
-}
+	const editorInstance = useEditorInstance();
+	const data = useDragHandleData();
+	const container = document.createElement("div");
+	injectExtension(useDragHandle({ element: container }));
 
-const activePos = ref<any>(null)
-let lockPosFun: any = null
+	onMounted(() => {
+		container.append(root.value);
+	});
 
-function update(editor:Editor, pos:number, dom: HTMLElement) {
-  if (showPop.value || showSlash.value) {
-    return
-  }
-  activePos.value = pos
-}
 
-onMounted(() => {
-  const {DragHandleExtension, lockPos} = useDragHandle({element: root.value, update}) as any
-  lockPosFun = lockPos
-  useExtension(DragHandleExtension)
-})
+	const items = ref([
+		{
+			name: "插入图片",
+			cmd: "/img",
+			icon: "image-line",
+			action: (range: Range) => {
+				editorInstance.value.chain().insertContentAt(range.to, "<p></p>").focus().run();
+				editorInstance.value.storage.image.openUploader();
+			},
+		},
+		{
+			name: "插入视频",
+			cmd: "/video",
+			icon: "video-line",
+			action: (range: Range) => {
+				editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").run();
+				editorInstance.value.storage.video.openUploader();
+			},
+		},
+		{
+			name: "引用",
+			cmd: "/b",
+			icon: "double-quotes-l",
+			action: (range: Range) => editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").toggleBlockquote().run(),
+		},
+		{
+			name: "标题1",
+			cmd: "/h1",
+			icon: "h-1",
+			action: (range: Range) => editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").setHeading({ level: 1 }).run(),
+		},
+		{
+			name: "标题2",
+			cmd: "/h2",
+			icon: "h-2",
+			action: (range: Range) => editorInstance.value.chain().insertContentAt(range.to, "<p></p>").setHeading({ level: 2 }).run(),
+		},
+		{
+			name: "标题3",
+			cmd: "/h3",
+			icon: "h-3",
+			action: (range: Range) => editorInstance.value.chain().insertContentAt(range.to, "<p></p>").setHeading({ level: 3 }).run(),
+		},
+		{
+			name: "列表",
+			cmd: "/list",
+			icon: "list-unordered",
+			action: (range: Range) => editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").toggleBulletList().run(),
+		},
+		{
+			name: "数学公式",
+			cmd: "/math",
+			icon: "functions",
+			action: (range: Range) => {
+				editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").run();
+				editorInstance.value.storage["hb-math"].openEditor();
+			},
+		},
+		{
+			name: "代码",
+			cmd: "/code",
+			icon: "brackets-line",
+			action: (range: Range) => editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").toggleCode().run(),
+		},
+		{
+			name: "代码块",
+			cmd: "/codeblock",
+			icon: "code-view",
+			action: (range: Range) => editorInstance.value.chain().focus().insertContentAt(range.to, "<p></p>").toggleCodeBlock().run(),
+		},
+	]);
+	const items2 = ref([
+		{
+			name: "上移一行",
+			icon: "arrow-up-s-line",
+			action: (range: Range) => {
+				if (!data.value.preRange) {
+					return;
+				}
+				const editor = editorInstance.value;
+				const state = editorInstance.value.state;
+				const tr = state.tr;
+				const range1 = { ...data.value.preRange }; // 第二个 range 的位置
+				const range2 = { ...range }; // 第一个 range 的位置
+				const fromNode = state.doc.cut(range1.from, range1.to);
+				const toNode = state.doc.cut(range2.from, range2.to);
+				tr.replaceRangeWith(range2.from, range2.to, fromNode);
+				tr.replaceRangeWith(range1.from, range1.to, toNode);
+				editor.view.dispatch(tr);
+			},
+		},
+		{
+			name: "删除本行", icon: "close-line", action: (range: Range) => {
+				const editor = editorInstance.value;
+				const state = editorInstance.value.state;
+				const tr = state.tr;
+				tr.delete(range.from, range.to).scrollIntoView();
+				editorInstance.value.view.dispatch(tr);
+			},
+		},
+		{
+			name: "下移一行",
+			icon: "arrow-down-s-line",
+			action: (range: Range) => {
+				if (!data.value.nextRange) {
+					return;
+				}
+				const editor = editorInstance.value;
+				const state = editorInstance.value.state;
+				const tr = state.tr;
+				const range1 = { ...range }; // 第一个 range 的位置
+				const range2 = { ...data.value.nextRange }; // 第二个 range 的位置
+				const fromNode = state.doc.cut(range1.from, range1.to);
+				const toNode = state.doc.cut(range2.from, range2.to);
+				tr.replaceRangeWith(range2.from, range2.to, fromNode);
+				tr.replaceRangeWith(range1.from, range1.to, toNode);
+				editor.view.dispatch(tr);
+			},
+		},
+	]);
 
-const items = ref([
-  {
-    name: '插入图片',
-    cmd: '/img',
-    icon: 'image-line',
-    action: (range: any) => {
-      editorInstance.value.chain().insertContentAt(range.to, '<p></p>').focus().run()
-      editorInstance.value.storage.image.openUploader()
-    }
-  },
-  {
-    name: '插入视频',
-    cmd: '/video',
-    icon: 'video-line',
-    action: (range: any) => {
-      editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').run()
-      editorInstance.value.storage.video.openUploader()
-    }
-  },
-  {
-    name: '引用',
-    cmd: '/b',
-    icon: 'double-quotes-l',
-    action: (range: any) => editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').toggleBlockquote().run()
-  },
-  {
-    name: '标题1',
-    cmd: '/h1',
-    icon: 'h-1',
-    action: (range: any) => editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').setHeading({level: 1}).run()
-  },
-  {
-    name: '标题2',
-    cmd: '/h2',
-    icon: 'h-2',
-    action: (range: any) => editorInstance.value.chain().insertContentAt(range.to, '<p></p>').setHeading({level: 2}).run()
-  },
-  {
-    name: '标题3',
-    cmd: '/h3',
-    icon: 'h-3',
-    action: (range: any) => editorInstance.value.chain().insertContentAt(range.to, '<p></p>').setHeading({level: 3}).run()
-  },
-  {
-    name: '列表',
-    cmd: '/list',
-    icon: 'list-unordered',
-    action: (range: any) => editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').toggleBulletList().run()
-  },
-  {
-    name: '数学公式',
-    cmd: '/math',
-    icon: 'functions',
-    action: (range: any) => {
-      editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').run()
-      editorInstance.value.storage['hb-math'].openEditor()
-    }
-  },
-  {
-    name: '代码',
-    cmd: '/code',
-    icon: 'brackets-line',
-    action: (range: any) => editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').toggleCode().run()
-  },
-  {
-    name: '代码块',
-    cmd: '/codeblock',
-    icon: 'code-view',
-    action: (range: any) => editorInstance.value.chain().focus().insertContentAt(range.to, '<p></p>').toggleCodeBlock().run()
-  },
-])
+	function doAction(e: any) {
+		if (data.value.rect) {
+			e.action(data.value.range);
+			showSlash.value = false;
+			showPop.value = false;
+		}
+	}
 
-const items2 = ref([
-  {
-    name: '上移一行',
-    icon: 'arrow-up-s-line',
-    action: (range: any, pos: any) => {
-      if (!pos.preRange) {
-        return
-      }
-      const editor = editorInstance.value
-      const state = editorInstance.value.state
-      const tr = state.tr
-      const range1 = {...pos.preRange}; // 第二个 range 的位置
-      const range2 = {...range}; // 第一个 range 的位置
-      const fromNode = state.doc.cut(range1.from, range1.to)
-      const toNode = state.doc.cut(range2.from, range2.to)
-      tr.replaceRangeWith(range2.from, range2.to, fromNode)
-      tr.replaceRangeWith(range1.from, range1.to, toNode)
-      editor.view.dispatch(tr)
-    }
-  },
-  {
-    name: '删除本行', icon: 'close-line', action: (range: any) => {
-      const editor = editorInstance.value
-      const state = editorInstance.value.state
-      const tr = state.tr;
-      tr.delete(range.from, range.to).scrollIntoView()
-      editorInstance.value.view.dispatch(tr);
-    }
-  },
-  {
-    name: '下移一行',
-    icon: 'arrow-down-s-line',
-    action: (range: any, pos: any) => {
-      if (!pos.nextRange) {
-        return
-      }
-      const editor = editorInstance.value
-      const state = editorInstance.value.state
-      const tr = state.tr
-      const range1 = {...range}; // 第一个 range 的位置
-      const range2 = {...pos.nextRange}; // 第二个 range 的位置
-      const fromNode = state.doc.cut(range1.from, range1.to)
-      const toNode = state.doc.cut(range2.from, range2.to)
-      tr.replaceRangeWith(range2.from, range2.to, fromNode)
-      tr.replaceRangeWith(range1.from, range1.to, toNode)
-      editor.view.dispatch(tr)
-    }
-  }
-])
-
-function doAction(e: any) {
-  if (activePos.value) {
-    e.action(activePos.value.range, activePos.value)
-    showSlash.value = false
-    showPop.value = false
-  }
-}
-
-watch([showSlash, showPop],()=>{
-  if (showSlash.value || showPop.value){
-    lockPosFun(true)
-  }else{
-    lockPosFun(false)
-  }
-})
+	watch([showSlash, showPop], () => {
+		if (showSlash.value || showPop.value) {
+			lockDragHandle();
+		} else {
+			unlockDragHandle();
+		}
+	});
 
 </script>
 
 <template>
-  <div class="drag-handle" ref="root">
-      <n-popover :z-index="99999" style="padding: 0" v-model:show="showSlash" trigger="click" placement="bottom-start"
-                 :show-arrow="false">
-        <template #trigger>
-          <div class="drag-button">
-            <i class="ri-add-fill"></i>
-          </div>
-        </template>
-        <slot name="drag-handle-slash" :activePos="activePos">
-          <n-element class="slash-command">
-            <div class="slash-item" v-for="(e, i) in items" @click="doAction(e)" :key="e.cmd">
-              <div class="slash-name">
-                <div class="slash-icon">
-                  <i :class="`ri-${e.icon}`"></i>
-                </div>
-                <span>{{ e.name }}</span>
-              </div>
-            </div>
-          </n-element>
-        </slot>
-      </n-popover>
-      <n-popover :z-index="99999" style="padding: 0" v-model:show="showPop" trigger="click" placement="bottom-start"
-                 :show-arrow="false">
-        <template #trigger>
-          <div class="drag-button">
-            <i class="ri-draggable"></i>
-          </div>
-        </template>
-        <slot name="drag-handle-select" :activePos="activePos">
-          <n-element class="slash-command">
-            <div class="slash-item" v-for="(e, i) in items2" @click="doAction(e)" :key="e.name">
-              <div class="slash-name">
-                <div class="slash-icon">
-                  <i :class="`ri-${e.icon}`"></i>
-                </div>
-                <span>{{ e.name }}</span>
-              </div>
-            </div>
-          </n-element>
-        </slot>
-      </n-popover>
-    </div>
+	<div style="display: none">
+		<div class="drag-handle" ref="root">
+			<n-popover :z-index="99999" style="padding: 0" v-model:show="showSlash" trigger="click" placement="bottom-start"
+								 :show-arrow="false">
+				<template #trigger>
+					<n-element class="drag-button">
+						<i class="ri-add-fill"></i>
+					</n-element>
+				</template>
+				<slot name="drag-handle-slash">
+					<n-element class="slash-command">
+						<div class="slash-item" v-for="(e, i) in items" @click="doAction(e)" :key="e.cmd">
+							<div class="slash-name">
+								<div class="slash-icon">
+									<i :class="`ri-${e.icon}`"></i>
+								</div>
+								<span>{{ e.name }}</span>
+							</div>
+						</div>
+					</n-element>
+				</slot>
+			</n-popover>
+			<n-popover :z-index="99999" style="padding: 0" v-model:show="showPop" trigger="click" placement="bottom-start"
+								 :show-arrow="false">
+				<template #trigger>
+					<n-element class="drag-button">
+						<i class="ri-draggable"></i>
+					</n-element>
+				</template>
+				<slot name="drag-handle-select">
+					<n-element class="slash-command">
+						<div class="slash-item" v-for="(e, i) in items2" @click="doAction(e)" :key="e.name">
+							<div class="slash-name">
+								<div class="slash-icon">
+									<i :class="`ri-${e.icon}`"></i>
+								</div>
+								<span>{{ e.name }}</span>
+							</div>
+						</div>
+					</n-element>
+				</slot>
+			</n-popover>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-.slash-command {
-  width: 160px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  outline: none;
-  border: none;
-  user-select: none;
-}
+	.slash-command {
+		width: 160px;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		outline: none;
+		border: none;
+		user-select: none;
+	}
 
-.slash-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 6px 10px;
-  transition: all 0.5s;
-}
+	.slash-item {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 6px 10px;
+		transition: all 0.5s;
+	}
 
-.slash-item:hover {
-  background: var(--hover-color);
-}
-
-
-.slash-name {
-  display: flex;
-  gap: 10px;
-  align-items: center;
-  font-size: 14px;
-}
-
-.slash-icon {
-  border: 1px solid var(--border-color);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 24px;
-  height: 24px;
-  border-radius: 5px;
-}
+	.slash-item:hover {
+		background: var(--hover-color);
+	}
 
 
-.drag-handle {
-  align-items: center;
-  display: none;
-  gap: 2px;
-}
+	.slash-name {
+		display: flex;
+		gap: 10px;
+		align-items: center;
+		font-size: 14px;
+	}
 
-.drag-button{
-  width: 24px;
-  height: 24px;
-  border-radius: 5px;
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: v-bind(vars.textColor3);
-}
+	.slash-icon {
+		border: 1px solid var(--border-color);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 24px;
+		height: 24px;
+		border-radius: 5px;
+	}
 
-.drag-button:hover {
-  background: v-bind(vars.hoverColor);
-  border: 1px solid v-bind(vars.borderColor);
-}
+
+	.drag-handle {
+		align-items: center;
+		gap: 2px;
+		display: flex;
+	}
+
+	.drag-button {
+		width: 24px;
+		height: 24px;
+		border-radius: 5px;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--text-color3);
+	}
+
+	.drag-button:hover {
+		background: var(--hover-color);
+		border: 1px solid var(--border-color);
+	}
 
 </style>
